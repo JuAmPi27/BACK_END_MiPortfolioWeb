813ebf93e499baad852aa672d498b1be30676a67
Juampi27 <Jpbarnetche27@gmail.com>
2022-05-21 17:08:50 -0300

    pequeña modificación

--- portfolioWeb/src/main/java/com/yoProgramo/portfolioWeb/model/Curso.java
+++ portfolioWeb/src/main/java/com/yoProgramo/portfolioWeb/model/Curso.java
@@ -22,22 +22,22 @@ public class Curso {
     private String nombre;
     private String lugar;
     private String duracion;
-    private String href;
 
     public Curso() {
     }
 
-    public Curso(String nombre, String lugar, String duracion, String href) {
+    public Curso(String nombre, String lugar, String duracion) {
         this.nombre = nombre;
         this.lugar = lugar;
         this.duracion = duracion;
-        this.href = href;
     }
 
     @Override
     public String toString() {
-        return "Curso{" + "id=" + id + ", nombre=" + nombre + ", lugar=" + lugar + ", duracion=" + duracion + ", href=" + href + '}';
+        return "Curso{" + "id=" + id + ", nombre=" + nombre + ", lugar=" + lugar + ", duracion=" + duracion + '}';
     }
+
+    
     
     
     
